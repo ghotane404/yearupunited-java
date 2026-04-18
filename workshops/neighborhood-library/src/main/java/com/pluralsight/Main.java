ce707caf41c2611fd4f9abbd6a7d134e5876650b
ghotane404 <62863408+ghotane404@users.noreply.github.com>
2026-04-17 19:49:09 -0700

    library project

--- workshops/neighborhood-library/src/main/java/com/pluralsight/Main.java
+++ workshops/neighborhood-library/src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,44 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+//        The Store Home Screen -
+//        • The home screen should display a list of options that a user can choose from.
+            System.out.println("Pick one of the options.\n" +
+                    "1) Show Available Books \n" +
+                    "2) Show Checked Out Books \n" +
+                    "3) Exit \n");
+            int userInput = scanner.nextInt();
+
+            switch (userInput) {
+                case 1:
+                    break;
+                case 2:
+                    System.out.println("Check Books Out");
+                    break;
+                case 3:
+                    System.out.println("Exit");
+                    break;
+            }
+
+        }
+    }
+
+//    • Show Available Books -
+//      Displays a list of all books that are NOT currently checked out.
+    public static void getAvailableBooks () {
+        System.out.println("The available list of books are: ");
+
+
+        System.out.println("What would you like to do? \n" +
+                "1) Select book to check out \n" +
+                "2) Exit to Home Screen \n");
+
+    }
 }
